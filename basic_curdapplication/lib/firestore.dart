62dbfceda5144e9efe89dea43818a2979b6aed51
Rafi076 <mohiuddinrafi076@gmail.com>
2024-09-30 22:46:43 +0600

    repear

--- basic_curdapplication/lib/firestore.dart
+++ basic_curdapplication/lib/firestore.dart
@@ -22,12 +22,18 @@ class FirestoreService {
     }
 
   // Update: update note given i a doc id
-  Future<void> updatesNote( String docID, String newNote){
-    return notes.doc(docID).update({
-      'note': newNote,
-      'timestamp': Timestamp.now(),
-    });
+  Future<void> updatesNote(String docID, String newNote) async {
+    try {
+      await notes.doc(docID).update({
+        'note': newNote,
+        'timestamp': Timestamp.now(),
+      });
+      print("Note updated successfully");
+    } catch (e) {
+      print("Error updating note: $e");
+    }
   }
+
 
   // Delete: Given notes given a doc id
    Future<void> deleteNote(String docID){
