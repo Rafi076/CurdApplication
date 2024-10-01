3f3ba289d6ec4a13c486ac2c0e69f22d672cf6e9
Rafi076 <mohiuddinrafi076@gmail.com>
2024-10-01 23:52:32 +0600

    darkmood set

--- basic_curdapplication/lib/home_page.dart
+++ basic_curdapplication/lib/home_page.dart
@@ -10,12 +10,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   // Firestore service instance
   final FirestoreService firestoreService = FirestoreService();
 
   // Text controller
   final TextEditingController textController = TextEditingController();
+
+  // Boolean for tracking dark mode
+  bool isDarkMode = false;
 
   // Open a dialog box to add or update a note
   void openNotebox(String? docID) {
@@ -53,94 +55,121 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  // Toggle between light and dark modes
+  void toggleDarkMode() {
+    setState(() {
+      isDarkMode = !isDarkMode;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'Add Motives',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    return MaterialApp(
+      themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      theme: ThemeData.light(), // Light theme
+      darkTheme: ThemeData.dark(), // Dark theme
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Add Motives',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: isDarkMode ? Colors.grey[900] : Colors.blue,
         ),
-        backgroundColor: Colors.blue,
-      ),
-      // Floating action button to add a new note
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => openNotebox(null), // Pass null for adding a new note
-        child: const Icon(Icons.add),
-        backgroundColor: Colors.blue,
-        shape: const CircleBorder(),
-      ),
+        // Floating action button to add a new note
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            // Add new note button
+            FloatingActionButton(
+              onPressed: () => openNotebox(null), // Pass null for adding a new note
+              child: const Icon(Icons.add),
+              backgroundColor: isDarkMode ? Colors.grey[800] : Colors.blue,
+              shape: const CircleBorder(),
+            ),
+            const SizedBox(height: 10),
+            // Toggle dark mode button
+            FloatingActionButton(
+              onPressed: toggleDarkMode, // Toggle dark mode
+              child: Icon(isDarkMode ? Icons.dark_mode : Icons.light_mode),
+              backgroundColor: isDarkMode ? Colors.grey[800] : Colors.blue,
+              shape: const CircleBorder(),
+            ),
+          ],
+        ),
 
-      body: StreamBuilder<QuerySnapshot>(
-        stream: firestoreService.getNoteStream(),
-        builder: (context, snapshot) {
-          // If we have data, get all the documents
-          if (snapshot.hasData) {
-            List notesList = snapshot.data!.docs;
+        body: StreamBuilder<QuerySnapshot>(
+          stream: firestoreService.getNoteStream(),
+          builder: (context, snapshot) {
+            // If we have data, get all the documents
+            if (snapshot.hasData) {
+              List notesList = snapshot.data!.docs;
 
-            // Display the notes in a ListView
-            return ListView.builder(
-              itemCount: notesList.length,
-              itemBuilder: (context, index) {
-                // Get each individual document
-                DocumentSnapshot document = notesList[index];
-                String docID = document.id;
+              // Display the notes in a ListView
+              return ListView.builder(
+                itemCount: notesList.length,
+                itemBuilder: (context, index) {
+                  // Get each individual document
+                  DocumentSnapshot document = notesList[index];
+                  String docID = document.id;
 
-                // Get note from each document
-                Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-                String noteText = data['note'];
+                  // Get note from each document
+                  Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                  String noteText = data['note'];
 
-                // Display each note inside a shadowed container
-                return Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 2,
-                          blurRadius: 5,
-                          offset: const Offset(0, 3), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                    child: ListTile(
-                      title: Text(
-                        noteText,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            onPressed: () => openNotebox(docID), // Open dialog to update the note
-                            icon: const Icon(Icons.settings),
-                          ),
-
-                          // Delete
-                          IconButton(
-                            onPressed: () => firestoreService.deleteNote(docID), // Delete the note
-                            icon: const Icon(Icons.delete),
+                  // Display each note inside a shadowed container
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: isDarkMode ? Colors.grey[850] : Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: const Offset(0, 3), // changes position of shadow
                           ),
                         ],
                       ),
+                      child: ListTile(
+                        title: Text(
+                          noteText,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              onPressed: () => openNotebox(docID), // Open dialog to update the note
+                              icon: const Icon(Icons.edit),
+                            ),
+
+                            // Delete
+                            IconButton(
+                              onPressed: () => firestoreService.deleteNote(docID), // Delete the note
+                              icon: const Icon(Icons.clear),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                );
-              },
-            );
-          } else {
-            return const Center(
-              child: Text("No notes found."),
-            );
-          }
-        },
+                  );
+                },
+              );
+            } else {
+              return const Center(
+                child: Text("No notes found."),
+              );
+            }
+          },
+        ),
       ),
     );
   }
